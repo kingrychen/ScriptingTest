2c537955189528977d80bb2b1d320784defb4924
S1ngS1ng <liuxing0514@gmail.com>
2016-03-09 00:12:56 -0600

    feat(main) add browser compatibility, add comments, add clear button

--- RockPaperScissors_S1ngS1ng/scripts/scripts.js
+++ RockPaperScissors_S1ngS1ng/scripts/scripts.js
@@ -3,13 +3,19 @@
  */
 angular.module('mainApp', [])
     .controller('mainCtrl', function ($scope) {
+        // Initialize data pattern
         $scope.buttonsValue = {
             0: "石头",
             1: "剪子",
             2: "布"
         };
-        var tryCounter = 0;
-        var correctCounter = 0;
+
+        // Initialize counter
+        var tryCounter = $scope.tryCounter = 0;
+        var correctCounter = $scope.correctCounter = 0;
+        $scope.winRate = '0 %';
+
+        // Initialize result pattern
         var resultPattern = {
             0: {
                 1: true,
@@ -24,25 +30,41 @@ angular.module('mainApp', [])
                 1: false
             }
         };
+
+        // Function of computer random choice
+        // Integer in range [0, 3)
         var computerRandom = function () {
             return Math.floor(Math.random() * 3);
         };
+
+        // Function of checking result, return $scope.result for UI
         var checkResult = function (usr, comp) {
 
-            if (usr == comp) $scope.result = '平手';
+            if (usr === comp) $scope.result = '平手';
             else $scope.result = resultPattern[usr][comp] ? '胜利' : '失败';
+            return $scope.result
         };
+
+        // Invoked when clicking option buttons
         $scope.showResult = function (userChoice) {
             var currentChoice = userChoice;
-            console.log(currentChoice);
             var computerChoice = computerRandom();
+            // Get output data with data pattern
             $scope.computerChoice = $scope.buttonsValue[computerChoice];
             checkResult(currentChoice, computerChoice);
             tryCounter += 1;
+            // Calculate for implementation
             $scope.tryCounter = tryCounter;
             if (resultPattern[currentChoice][computerChoice] == true) correctCounter += 1;
             $scope.correctCounter = correctCounter;
-            $scope.winRate = (correctCounter/tryCounter).toFixed(2) * 100 + '%';
+            $scope.winRate = (correctCounter/tryCounter*100).toString().split('.')[0] + '%';
         };
+
+        // Invoked when click 'clear' button
+        $scope.clear = function () {
+            $scope.tryCounter = 0;
+            $scope.correctCounter = 0;
+            $scope.winRate = '0 %';
+        }
 
     });
